f983f581826fc36e60329ca60767757b4bedcef3
Hadassa511 <hadassa511.m@gmail.com>
2019-12-16 01:28:17 -0500

    Initial Commit

---
+++ client/build/precache-manifest.15e4e2fad72acf6bf3a7434a7085c450.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54a15573258f087e330e9f11966cbd6",
+    "url": "/Challenge/index.html"
+  },
+  {
+    "revision": "41d191533148fb2ba165",
+    "url": "/Challenge/static/css/main.ff110be4.chunk.css"
+  },
+  {
+    "revision": "44becd88bdb3a41b0e0e",
+    "url": "/Challenge/static/js/2.b4499aae.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/Challenge/static/js/2.b4499aae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41d191533148fb2ba165",
+    "url": "/Challenge/static/js/main.d520ef67.chunk.js"
+  },
+  {
+    "revision": "deb2bdde6e7deec174ad",
+    "url": "/Challenge/static/js/runtime-main.2b08d088.js"
+  }
+]);
